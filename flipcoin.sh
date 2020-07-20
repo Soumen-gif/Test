12dc9add91923c8f8269e31cdba2258ed75cc5cd
soumen-gif <soumentaki1996@gmail.com>
2020-07-20 21:26:53 +0530

    single flip coin and percentage

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,10 +1,32 @@
-#! /bin/bash -x
+#!/bin/bash
 
-flipCoin=$((RANDOM%2))
+echo "flip Coin Simulator"
 
-if [ $flipCoin -eq 1 ]
+echo "number of times user want to flip the coin"
+read flip
+echo $flip
+
+countHead=0
+countTail=0
+declare -A noOfCounts
+for((i=0;i<flip;i++))
+do
+if [ $((RANDOM%2)) -eq 1 ]
 then
-    echo "TAIL"
+((countHead++))
 else
-    echo "HEAD"
+((countTail++))
 fi
+done
+echo "head="$countHead
+echo "tail="$countTail
+
+noOfCounts[tail]=$countTail
+noOfCounts[head]=$countHead
+
+echo ${noOfCounts[@]}
+
+PercentageOfTails=$((100*$countTail/$flip))
+echo "Percentage of tails= "$PercentageOfTails
+PercentageOfHeads=$((100*$countHead/$flip))
+echo "Percentage of heads= "$PercentageOfHeads
